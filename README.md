ff7100a9a3f7ab1c406de5768beed564e0841dd8
Felix Toledo <felixtoledoctes@gmail.com>
2022-08-23 16:03:53 -0300

    Challenged Finished

---
+++ README.md
@@ -0,0 +1,80 @@
+# Frontend Mentor - Stats preview card component solution
+
+This is a solution to the [ Interactive card details form coding challenge](https://www.frontendmentor.io/challenges/interactive-card-details-form-XpS8cKZDWw). 
+But I also change it a little bit because I'm learning JS and I needed it to use as an excercise for what I learned.
+This solution was made with HTML, CSS and JS VANILLA.
+
+********************////////////////#################DO NOT COMPLETE THE PAGE WITH YOUR REAL CARD DATA############////////////////***********************
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Your challenge is to build out this interactive card details form and get it looking as close to the design as possible.
+
+You can use any tools you like to help you complete the challenge. So if you've got something you'd like to practice, feel free to give it a go.
+
+Your users should be able to: 
+
+- Fill in the form and see the card details update in real-time
+- Receive error messages when the form is submitted if:
+  - Any input field is empty
+- See hover, active, and focus states for interactive elements on the page
+
+Also I added some challenges:
+- Save in a local storage the cards you added.
+- Can choose the card you want to see the details.
+
+### Screenshot
+Desktop:
+![](./screenshot_1.png)
+
+### Links
+
+- Solution URL: [CODE]()
+- Live Site URL: [DEPLOYMENT]()
+
+## My process
+
+### Built with
+
+- CSS
+- JavaScript
+- HTML
+
+### What I learned
+
+I learned some JS things on a course to start my first JS frontend mentor challenge.
+
+- Flow Controls
+- Cycles and iterations.
+- Functions
+- Objects and Arrays.
+- High Order Functions.
+- DOM.
+- Events.
+- Storage & JSON.
+
+### Useful resources
+
+- [CODERHOUSE](https://www.coderhouse.com/online/javascript) - This is the place where I am studying.
+- [Free Code Camp](https://www.freecodecamp.org/) - Free Code Camp helped me when I was starting at JS.
+- [Argentina Programa](https://www.argentina.gob.ar/produccion/transformacion-digital-y-economia-del-conocimiento/argentina-programa) - A public course for the Argentinian citizens.
+
+## Author
+
+- GitHub - [Felix Toledo](https://github.com/felix-toledo)
+- Frontend Mentor - [@felix-toledo](https://www.frontendmentor.io/profile/felix-toledo)
+- Twitter - [@_felixtoledo](https://www.twitter.com/_felixtoledo)
---
+++ script/app.js
@@ -0,0 +1,118 @@
+class tarjeta{
+    constructor(number, name, cvc, month, year, times){
+        this.number = number;
+        this.name = name;
+        this.cvc = cvc;
+        this.month = month;
+        this.year = year;
+        this.times = times;
+    }
+}
+let tarjetas = [];
+
+if(localStorage.getItem("tarjetas")){
+    tarjetas = JSON.parse(localStorage.getItem("tarjetas"))
+    console.log(tarjetas)
+}else{
+    console.log(`primera vez que carga tarjetas`)
+    localStorage.setItem("tarjetas", JSON.stringify(tarjetas))
+}
+
+let inputName = document.getElementById("name");
+let inputNumber = document.getElementById("cardNumber");
+let inputMonth = document.getElementById("MM");
+let inputYear = document.getElementById("YY");
+let inputCVC = document.getElementById("cvc");
+let contTarjetasGuard = document.getElementById("contenedorTarjetasGuardadas");
+let success = document.getElementById("completeAdded");
+let botonCont = document.getElementById("btnCont");
+let botonVerT = document.getElementById("verTarjetas");
+let boton = document.getElementById("btnConf");
+let spaceName = document.getElementById("inCardName");
+let spaceNumber = document.getElementById("inCardNumber");
+let spaceExpDate = document.getElementById("inCardExpDate");
+let spaceCVC = document.getElementById("inCardCVC");
+
+inputName.addEventListener("input", ()=>{inputChanged()});
+inputNumber.addEventListener("input", ()=>{inputChanged()});
+inputMonth.addEventListener("input", ()=>{inputChanged()});
+inputYear.addEventListener("input", ()=>{inputChanged()});
+inputCVC.addEventListener("input", ()=>{inputChanged()});
+boton.addEventListener("click", (event)=> {guardarTarjeta(event)});
+botonCont.addEventListener("click", ()=> {success.setAttribute("class", "succesAdded_no")});
+botonVerT.addEventListener("click", ()=> {verTarjetas("si")});
+
+function inputChanged(){
+    spaceName.innerHTML = `<span class="inCardName" id="inCardName">${inputName.value.toUpperCase()}</span> `
+    if (inputName.value == ""){
+        spaceName.innerHTML = ` <span class="inCardName" id="inCardName">FELIX TOLEDO</span> `
+    }
+
+    spaceNumber.innerHTML = `<p class="inCardNumber" id="inCardNumber">${inputNumber.value}</p>`
+    if (inputNumber.value == ""){
+        spaceNumber.innerHTML = `<p class="inCardNumber" id="inCardNumber">0000 0000 0000 0000</p>`
+    }
+
+    spaceExpDate.innerHTML = `<span class="inCardExpDate" id="inCardExpDate">${inputMonth.value}/${inputYear.value}</span>`
+    if (inputMonth.value == "" && inputYear.value == ""){
+        spaceExpDate.innerHTML = `<span class="inCardExpDate" id="inCardExpDate">00/00</span>`
+    }
+
+    spaceCVC.innerHTML = `<p class="inCardCVC" id="inCardCVC">${inputCVC.value}</p>`
+    if (inputCVC == ""){
+        spaceCVC.innerHTML = `<p class="inCardCVC" id="inCardCVC">000</p>`
+    }
+}
+
+function guardarTarjeta(event){
+    event.preventDefault()
+    if(inputCVC == "" || inputMonth.value == "" || inputYear.value == "" || inputNumber.value == "" || inputName.value == ""){
+        alert("faltan datos");
+    } else {
+        tarjetas.push(new tarjeta(inputNumber.value, inputName.value, inputCVC.value, inputMonth.value, inputYear.value, 0));
+        localStorage.setItem("tarjetas", JSON.stringify(tarjetas));
+        success.setAttribute("class", "succesAdded");
+        limpiarDatos();
+    }
+}
+
+function verTarjetas(param){
+    if(param == "si"){
+        contTarjetasGuard.setAttribute("class", "contenedorTarjetasGuardadas");
+        botonVerT.addEventListener("click", ()=> {verTarjetas("no");});
+        mostrarTarjetas();
+    } else {
+        contTarjetasGuard.setAttribute("class", "contenedorTarjetasGuardadas_no");
+        botonVerT.addEventListener("click", ()=> {verTarjetas("si");});
+    }
+}
+
+function mostrarTarjetas(){
+    contTarjetasGuard.innerHTML= "";
+    tarjetas.forEach((tarjet)=>{
+        console.log(tarjet)
+        let {number, name} = tarjet;
+        let nuevaTarjeta = document.createElement("div");
+        nuevaTarjeta.innerHTML = `    <div class="tarjetaGuardada" id="${number.substr(number.length - 4)}"><span>${number.substr(number.length - 4)}</span><p>${name}</p></div>`
+        contTarjetasGuard.appendChild(nuevaTarjeta)
+        let btnUsar = document.getElementById(`${number.substr(number.length - 4)}`)
+        btnUsar.addEventListener("click", () =>{usar(tarjet)});
+        })
+}
+
+function usar(tarjet){
+    console.log(tarjet)
+    let {number, name, cvc, month, year} = tarjet;
+    spaceName.innerHTML = `<span class="inCardName" id="inCardName">${name.toUpperCase()}</span> `
+    spaceNumber.innerHTML = `<p class="inCardNumber" id="inCardNumber">${number}</p>`
+    spaceExpDate.innerHTML = `<span class="inCardExpDate" id="inCardExpDate">${month}/${year}</span>`
+    spaceCVC.innerHTML = `<p class="inCardCVC" id="inCardCVC">${cvc}</p>`
+}
+
+function limpiarDatos(){
+    inputName.value = "";
+    inputNumber.value = "";
+    inputMonth.value = "";
+    inputYear.value = "";
+    inputCVC.value = "";
+}
